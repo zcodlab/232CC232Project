5811e481943e198e745ec869091733d874dd9420
inwe-research-group <zmamanir@unmsm.edu.pe>
2023-09-25 19:15:09 -0500

    Add SearchObject Class and Modified Persona Class

--- src/main/java/uni/aed/model/Persona.java
+++ src/main/java/uni/aed/model/Persona.java
@@ -1,82 +1,96 @@
 package uni.aed.model;
-public class Persona {
-    public static final int NAME=0;
-    public static final int AGE=1;
+/**
+ *
+ * @author hp
+ */
+public class Persona implements Comparable{
     
-    public static final int LESS=-1;
-    public static final int EQUAL=0;
-    public static final int MORE=1;
-    
-    public static int compareAttribute;    
-    
-    private String name;
-    private int age;
-    private char gender;
-    
-    static{
-        compareAttribute=NAME;
+    public static final int NAME = 0;
+    public static final int AGE = 1;    
+    private static final int LESS = -1;
+    private static final int EQUAL = 0;    
+    private static final int MORE  = 1;    
+    private static int compareAttribute;    
+    private String  name;
+    private int     age;
+    private char    gender;
+    static {
+       compareAttribute = NAME;       
     }
-
+    public Persona() {
+        this("No Ingresado", 0, 'U');
+    }
     public Persona(String name, int age, char gender) {
-        this.name = name;
-        this.age = age;
+        this.age    = age;
+        this.name   = name;
         this.gender = gender;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public char getGener() {
-        return gender;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
+    }        
+      
+    public void setAge( int age ) {
         this.age = age;
     }
-
-    public void setGener(char gender) {
-        this.gender = gender;
-    }
-
     
-    @Override
+    public void setGender( char gender ) {
+        this.gender = gender;
+    }    
+    
+    public void setName( String name ) {
+        this.name = name;
+    }
+    
+     public int getAge( ) {
+        return age;
+    }    
+    
+    public char getGender( ) {
+        return gender;
+    }    
+    
+    public String getName( ) {
+        return name;
+    }    
+    
     public String toString( )  {
         return this.name    + "||" +
                this.age     + "||" +
                this.gender;
     }
-    
-    public void setCompareAttribute(int attribute){
-        compareAttribute=attribute;
+    public String toString1( )  {
+        return this.name    + "\t\t" +
+               this.age     + "\t\t" +
+               this.gender;
     }
-    public int compareTo(Persona persona){
+    public void setCompareAttribute( int attribute ) {
+        compareAttribute = attribute;
+    }    
+    public int compareTo( Persona persona, int attribute ) {
+        int comparisonResult;
+
+        if ( attribute == AGE ) {
+            int p2age = persona.getAge( );
+
+            if (this.age < p2age) {
+                comparisonResult = LESS;
+            } else if (this.age == p2age) {
+                comparisonResult = EQUAL;
+            } else {
+                assert this.age > p2age;
+                comparisonResult = MORE;
+            }
+
+        } else { //compara el nombre usando el metodo compareTo de la clase String                
+            String    p2name = persona.getName( );
+            comparisonResult = this.name.compareTo(p2name);
+        }
+
+        return comparisonResult;
+    }        
+    public int compareTo( Persona persona ) {
         return compareTo(persona, compareAttribute);
     }
-    public int compareTo(Persona persona, int attribute){
-        int comparisonResult;
-        if(attribute==AGE){
-            int p2age=persona.getAge();
-            if(this.age<p2age)
-                comparisonResult=LESS;
-            else if(this.age==p2age)
-                comparisonResult=EQUAL;
-            else
-                comparisonResult=MORE;
-        }else{
-            String p2name=persona.getName();
-            comparisonResult=this.name.compareTo(p2name);
-        }
-        return comparisonResult;
-    }
 
-    
+    @Override
+    public int compareTo(Object o) {
+        return compareTo((Persona)o, compareAttribute);
+    }
 }
